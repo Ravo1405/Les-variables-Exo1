230c17fda279bad4e100f4bc98435bad763f0136
Ravo1405 <ravonirina.rasolofomanana@yahoo.com>
2022-08-21 18:50:10 +0200

    ajout fichier index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//Créer une variable name et l'initialiser avec la valeur de votre choix.  Afficher son contenu.
+//Déclaration de la variable et initialisation de cette variable avec une chaîne de caractère.
+$nameVarible = "Toto"
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les variables</title>
+</head>
+<body>
+    <!--Affichage du contenu de la variable nameVariable dans une balise HTML p via un echo PHP. -->
+    <p><?php echo $nameVarible; ?></p>
+</body>
+</html>
